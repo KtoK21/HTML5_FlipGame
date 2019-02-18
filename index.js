67f4237a171c04791b70f0c56c315154499f0ec9
root <root@KtoK21.localdomain>
2019-02-18 18:53:35 +0900

    NodeJS test2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const PORT = process.env.PORT;
 const app = express();
 app.get("/", (req, res) => {
-res.send({ hello: "world" });
+res.send({test);
 });
 app.listen(PORT);
